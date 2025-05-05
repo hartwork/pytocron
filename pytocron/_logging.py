db37acc50eb674f4770c5677e940f23480a6fc46
Sebastian Pipping <sebastian@pipping.org>
2025-05-05 17:29:57 +0200

    _logging.py: Start reporting on local time zone

--- pytocron/_logging.py
+++ pytocron/_logging.py
@@ -6,6 +6,8 @@
 import logging
 import sys
 
+from ._timing import epoch_to_local_datetime
+
 LOG_LEVELS = {
     "DEBUG": logging.DEBUG,
     "ERROR": logging.ERROR,
@@ -13,9 +15,22 @@ LOG_LEVELS = {
 }
 
 
+class _CustomFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):  # noqa: ARG002, N802
+        dt = epoch_to_local_datetime(record.created)
+        return dt.isoformat(sep=" ", timespec="milliseconds")
+
+
 def configure_logging(level_name: str) -> None:
+    format_ = "pytocron [%(asctime)s] %(levelname)s: %(message)s"
+
     logging.basicConfig(
         level=LOG_LEVELS[level_name],
         stream=sys.stderr,
-        format="pytocron [%(asctime)s] %(levelname)s: %(message)s",
+        format=format_,
     )
+
+    formatter = _CustomFormatter(fmt=format_)
+
+    for handler in logging.root.handlers:
+        handler.setFormatter(formatter)
